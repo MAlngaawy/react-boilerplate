3712a16da530fbf9be023c020cf98e3b466a6457
MAlngaawy <m.alngaawy@gmail.com>
2023-03-04 05:34:02 +0200

    handle active links

--- src/@core/Components/Toolbar.jsx
+++ src/@core/Components/Toolbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import cn from "classnames";
 
 const ToolBar = () => {
   return (
-    <nav className="bg-blue-500 p-4">
+    <nav className="bg-blue-500 px-6">
       <ul className="flex gap-4 text-md text-white">
         <SingleLink to={"/"} text="Home" />
         <SingleLink to={"/signup"} text="Sign Up" />
@@ -15,9 +16,16 @@ const ToolBar = () => {
 export default ToolBar;
 
 const SingleLink = ({ to, text }) => {
+  const location = useLocation();
+  const active = location.pathname === to;
   const navigate = useNavigate();
   return (
-    <li className=" cursor-pointer" onClick={() => navigate(to)}>
+    <li
+      className={cn(" cursor-pointer px-2 py-3", {
+        "bg-white text-blue-500": active,
+      })}
+      onClick={() => navigate(to)}
+    >
       {text}
     </li>
   );
